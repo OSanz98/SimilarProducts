2461cb73fd71de4ebbb83600df5f1100ec7d98b6
OSanz98 <115473440+OSanz98@users.noreply.github.com>
2023-02-23 23:25:24 +0000

    Delete server directory

--- server/models/product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const productSchema = {
-    title: String,
-    url: String
-}
-const Product = mongoose.model("products", productSchema);
-
-module.exports = Product;
--- server/routes/prod/products.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const productApp = express();
-
-
-productApp.route('/products')
-.get('/products', (req, res) => {
-    
-})
